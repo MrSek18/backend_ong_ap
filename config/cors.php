cb86d6b57da33d9d17349603ebcb1c6bd82f42db
kevin <mrsek2022@gmail.com>
2025-12-21 16:16:13 -0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    // 'allowed_origins' => ['http://localhost:5173'], // tu frontend Vite
+    'allowed_origins' => ['https://localhost:5173', 'https://ong-frontend-production.vercel.app'],
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'Origin'],
     'exposed_headers' => [],
