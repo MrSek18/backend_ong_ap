8abf0b4879fcc7846a05c85dcfb225f2ac5530d7
kevin <mrsek2022@gmail.com>
2025-12-22 17:03:19 -0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'], 
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://ong-frontend-production.vercel.app', 'https://*.ngrok-free.app'],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://ong-frontend-production.vercel.app',
+    ],
+    'allowed_origins_patterns' => ['/https:\/\/.*\.ngrok-free\.app/'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
