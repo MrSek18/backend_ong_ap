15b7d6b0582d0388582edbb6d8f251ae47824eee
kevin <mrsek2022@gmail.com>
2025-12-22 16:26:50 -0500

    appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,16 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use MercadoPago\MercadoPagoConfig;
-
+use Illuminate\Support\Facades\Log;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register()
+{
+    $token = env('MERCADOPAGO_ACCESS_TOKEN');
+    if (!empty($token)) {
+        \MercadoPago\MercadoPagoConfig::setAccessToken($token);
+    } else {
+        Log::warning("MercadoPago Access Token no definido en build");
     }
+}
+
 
     /**
      * Bootstrap any application services.
