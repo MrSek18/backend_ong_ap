b846ef83cf012faf8f2e486966940e67f5af85eb
kevin <mrsek2022@gmail.com>
2025-12-22 16:06:52 -0500

    api.php health modified

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 Route::post('/pago', [PagoController::class, 'crearPago']);
-Route::post('/webhook', [PagoController::class, 'webhook']);}
+Route::post('/webhook', [PagoController::class, 'webhook']);
 
 // new health
 Route::get('/health', function () {
